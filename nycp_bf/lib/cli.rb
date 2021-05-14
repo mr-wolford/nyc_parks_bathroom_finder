bd95a42f73654a04ad313dc012fbadbd56e5d441
Matt Wolford <matthew.r.wolford@gmail.com>
2021-05-13 22:40:07 -0400

    changes to CLI

--- nycp_bf/lib/cli.rb
+++ nycp_bf/lib/cli.rb
@@ -2,8 +2,11 @@
 class NycpBf::CLI
     
     def call
-        puts "Welcome to the NYC Parks Bathroom Finder!"
-        binding.pry
+        puts "\nWelcome to the NYC Parks Bathroom Finder!"
+        # need to ask for zip code
+        # then take that zip and put it in the NYC Parks website search URL at the appropriate place
+        puts "\nPlease enter the NYC ZIP code where you would like to find a public park bathroom."
+        gets.strip
     end
 
 end
